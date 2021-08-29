02efefc87fa2a8be6860da89826a2c5c61c880ec
NTTC-kkuribara <kattun.kuribara@ntt.com>
2021-08-29 19:04:33 +0900

    Initial Commit

---
+++ FlappyBird/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlappyBird
+//
+//  Created by ITMS NTTcom on 2021/08/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
